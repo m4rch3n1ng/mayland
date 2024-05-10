643610b587020537a28a4ff615f0f44a18b71e05
may <m4rch3n1ng@gmail.com>
2024-05-10 20:20:16 +0200

    changes

--- mayctl/src/main.rs
+++ mayctl/src/main.rs
@@ -1,5 +1,4 @@
 use mayland::{action::Action, comm::Event};
-use postcard::to_allocvec;
 use std::{io::Write, os::unix::net::UnixStream};
 
 const SOCKET_PATH: &str = "/tmp/mayland.sock";
@@ -8,8 +7,8 @@ fn main() {
 	let action = Action::Workspace(2);
 	let event = Event::Dispatch(action);
 
-	let v = to_allocvec(&event).unwrap();
+	let v = postcard::to_stdvec(&event).unwrap();
 
-	let mut unix_stream = UnixStream::connect(SOCKET_PATH).unwrap();
-	unix_stream.write_all(&v).unwrap();
+	let mut stream = UnixStream::connect(SOCKET_PATH).unwrap();
+	stream.write_all(&v).unwrap();
 }
