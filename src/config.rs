ba75fbd606572d0cc480383c5498e01751a350e5
may <m4rch3n1ng@gmail.com>
2024-05-28 23:55:47 +0200

    print config errors

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,6 @@
 use self::{bind::Binds, input::Input};
-use crate::state::State;
+use crate::{error::MaylandError, state::State};
+use annotate_snippets::{Level, Renderer, Snippet};
 use serde::Deserialize;
 
 mod bind;
@@ -15,9 +16,34 @@ pub struct Config {
 const CONFIG_PATH: &str = "/home/may/.config/mayland.mf";
 
 impl Config {
-	pub fn read() -> Self {
-		let file = std::fs::read_to_string(CONFIG_PATH).unwrap();
-		mayfig::from_str(&file).unwrap()
+	pub fn read() -> Result<Self, MaylandError> {
+		let file = std::fs::read_to_string(CONFIG_PATH)
+			.map_err(|_| MaylandError::FileNotFound(CONFIG_PATH.to_owned()))?;
+
+		// workaround for https://github.com/rust-lang/annotate-snippets-rs/issues/25
+		let file = file.replace('\t', "    ");
+
+		match mayfig::from_str(&file) {
+			Ok(config) => Ok(config),
+			Err(err) => {
+				let code = err.code().to_string();
+				let message = if let Some(span) = err.span() {
+					Level::Error.title(&code).snippet(
+						Snippet::source(&file)
+							.origin(CONFIG_PATH)
+							.fold(true)
+							.annotation(Level::Error.span(span.range())),
+					)
+				} else {
+					Level::Error.title(&code)
+				};
+
+				let renderer = Renderer::styled();
+				anstream::println!("{}", renderer.render(message));
+
+				Err(MaylandError::AlreadyPrinted)
+			}
+		}
 	}
 }
 
