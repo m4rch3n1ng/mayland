253b074143f51be5e8d9bbef8a51989c01998df6
may <m4rch3n1ng@gmail.com>
2024-06-05 21:11:34 +0200

    touchpad config options

--- src/config/input.rs
+++ src/config/input.rs
@@ -1,11 +1,13 @@
 use crate::State;
 use serde::Deserialize;
 use smithay::input::keyboard::XkbConfig;
+use smithay::reexports::input as libinput;
 
 #[derive(Debug, Default, Deserialize)]
 #[serde(default)]
 pub struct Input {
 	pub keyboard: Keyboard,
+	pub touchpad: Touchpad,
 }
 
 #[derive(Debug, Deserialize)]
@@ -57,6 +59,95 @@ impl Default for Keyboard {
 
 			repeat_delay: 600,
 			repeat_rate: 25,
+		}
+	}
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(default)]
+pub struct Touchpad {
+	pub tap: bool,
+	pub tap_drag: bool,
+
+	pub dwt: bool,
+
+	pub click_method: ClickMethod,
+
+	pub natural_scroll: bool,
+	pub scroll_method: ScrollMethod,
+
+	pub accel_profile: AccelProfile,
+}
+
+impl Default for Touchpad {
+	fn default() -> Self {
+		Touchpad {
+			tap: true,
+			tap_drag: true,
+
+			dwt: true,
+
+			click_method: ClickMethod::Clickfinger,
+
+			natural_scroll: true,
+			scroll_method: ScrollMethod::TwoFinger,
+
+			accel_profile: AccelProfile::Adaptive,
+		}
+	}
+}
+
+#[derive(Debug, Clone, Copy, Default, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum ClickMethod {
+	ButtonAreas,
+	#[default]
+	Clickfinger,
+}
+
+impl From<ClickMethod> for libinput::ClickMethod {
+	fn from(value: ClickMethod) -> Self {
+		match value {
+			ClickMethod::ButtonAreas => libinput::ClickMethod::ButtonAreas,
+			ClickMethod::Clickfinger => libinput::ClickMethod::Clickfinger,
+		}
+	}
+}
+
+#[derive(Debug, Clone, Copy, Default, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum ScrollMethod {
+	NoScroll,
+	#[default]
+	TwoFinger,
+	Edge,
+	OnButtonDown,
+}
+
+impl From<ScrollMethod> for libinput::ScrollMethod {
+	fn from(value: ScrollMethod) -> Self {
+		match value {
+			ScrollMethod::NoScroll => libinput::ScrollMethod::NoScroll,
+			ScrollMethod::TwoFinger => libinput::ScrollMethod::TwoFinger,
+			ScrollMethod::Edge => libinput::ScrollMethod::Edge,
+			ScrollMethod::OnButtonDown => libinput::ScrollMethod::OnButtonDown,
+		}
+	}
+}
+
+#[derive(Debug, Clone, Copy, Default, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum AccelProfile {
+	#[default]
+	Adaptive,
+	Flat,
+}
+
+impl From<AccelProfile> for libinput::AccelProfile {
+	fn from(value: AccelProfile) -> Self {
+		match value {
+			AccelProfile::Adaptive => libinput::AccelProfile::Adaptive,
+			AccelProfile::Flat => libinput::AccelProfile::Flat,
 		}
 	}
 }
