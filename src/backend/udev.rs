e58d5190f510dc0db68c8a1662616f07766ab886
may <m4rch3n1ng@gmail.com>
2024-08-26 16:10:46 +0200

    full touchpad config support

--- src/backend/udev.rs
+++ src/backend/udev.rs
@@ -514,7 +514,7 @@ impl Udev {
 }
 
 impl State {
-	fn handle_libinput_event(&mut self, event: &mut InputEvent<LibinputInputBackend>) {
+	fn handle_libinput_event(&self, event: &mut InputEvent<LibinputInputBackend>) {
 		if let InputEvent::DeviceAdded { device } = event {
 			let config = &self.mayland.config;
 
@@ -523,17 +523,35 @@ impl State {
 				let conf = &config.input.touchpad;
 
 				let _ = device.config_tap_set_enabled(conf.tap);
-				let _ = device.config_tap_set_drag_enabled(conf.tap_drag);
+				let _ = device.config_tap_set_drag_enabled(conf.tap_and_drag);
+				let _ = device.config_tap_set_drag_enabled(conf.tap_drag_lock);
+
+				let _ = device.config_dwt_set_enabled(conf.dwt);
+				let _ = device.config_dwtp_set_enabled(conf.dwtp);
 
 				let click_method = smithay::reexports::input::ClickMethod::from(conf.click_method);
 				let _ = device.config_click_set_method(click_method);
 
-				let accel_profile = smithay::reexports::input::AccelProfile::from(conf.accel_profile);
-				let _ = device.config_accel_set_profile(accel_profile);
+				let _ = device.config_middle_emulation_set_enabled(conf.middle_emulation);
+				let tap_button_map = smithay::reexports::input::TapButtonMap::from(conf.tap_button_map);
+				let _ = device.config_tap_set_button_map(tap_button_map);
+				let _ = device.config_left_handed_set(conf.left_handed);
 
 				let _ = device.config_scroll_set_natural_scroll_enabled(conf.natural_scroll);
 				let scroll_method = smithay::reexports::input::ScrollMethod::from(conf.scroll_method);
 				let _ = device.config_scroll_set_method(scroll_method);
+
+				let accel_speed = conf.accel_speed.clamp(-1., 1.);
+				if accel_speed != conf.accel_speed {
+					tracing::warn!(
+						"invalid accel_speed {}, clamping to {}",
+						conf.accel_speed,
+						accel_speed
+					);
+				}
+				let _ = device.config_accel_set_speed(accel_speed);
+				let accel_profile = smithay::reexports::input::AccelProfile::from(conf.accel_profile);
+				let _ = device.config_accel_set_profile(accel_profile);
 			}
 		}
 	}
