8c00be107ba7c338458a1cce7ca0521e66f2e6fd
may <m4rch3n1ng@gmail.com>
2024-10-04 09:40:32 +0200

    use adjacently tagged enum representation for mayland-comm
    this, once more, duplicates the `Action` enum, as
    mayland-config wants to have externally tagged enums
    for the tagged enum representation instead of a struct

--- mayland-comm/src/action.rs
+++ mayland-comm/src/action.rs
@@ -2,9 +2,9 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
+#[serde(tag = "tag", content = "val")]
 pub enum Action {
 	Quit,
-	#[serde(alias = "close")]
 	CloseWindow,
 
 	Workspace(usize),
---
+++ mayland-config/src/bind/action.rs
@@ -0,0 +1,42 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Action {
+	Quit,
+	#[serde(alias = "close")]
+	CloseWindow,
+
+	Workspace(usize),
+
+	Spawn(Vec<String>),
+}
+
+impl From<Action> for mayland_comm::Action {
+	fn from(action: Action) -> Self {
+		match action {
+			Action::Quit => mayland_comm::Action::Quit,
+			Action::CloseWindow => mayland_comm::Action::CloseWindow,
+
+			Action::Workspace(workspace) => mayland_comm::Action::Workspace(workspace),
+
+			Action::Spawn(spawn) => mayland_comm::Action::Spawn(spawn),
+		}
+	}
+}
+
+impl From<mayland_comm::Action> for Action {
+	/// this implementation is not strictly necessary and should
+	/// probably not be used, but it exists so that the compiler warns
+	/// if the two enums are out of sync
+	fn from(action: mayland_comm::Action) -> Self {
+		match action {
+			mayland_comm::Action::Quit => Action::Quit,
+			mayland_comm::Action::CloseWindow => Action::CloseWindow,
+
+			mayland_comm::Action::Workspace(workspace) => Action::Workspace(workspace),
+
+			mayland_comm::Action::Spawn(spawn) => Action::Spawn(spawn),
+		}
+	}
+}
