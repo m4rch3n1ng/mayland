ac703e89cef5f0645ea4811a2049b350b71b05b4
may <m4rch3n1ng@gmail.com>
2024-11-13 00:04:53 +0100

    deserialize colors as #021caf type hex color strings

--- mayland-config/src/decoration.rs
+++ mayland-config/src/decoration.rs
@@ -1,4 +1,4 @@
-use serde::Deserialize;
+use serde::{de::Visitor, Deserialize};
 
 #[derive(Debug, Default, Deserialize)]
 #[serde(default)]
@@ -24,7 +24,7 @@ impl Default for Focus {
 	}
 }
 
-#[derive(Debug, Clone, Copy, Deserialize)]
+#[derive(Debug, Clone, Copy)]
 pub struct Color([u8; 3]);
 
 impl Color {
@@ -39,5 +39,53 @@ impl Color {
 			self.0[1] as f32 / 255.0,
 			self.0[2] as f32 / 255.0,
 		]
+	}
+}
+
+impl<'de> Deserialize<'de> for Color {
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+	where
+		D: serde::Deserializer<'de>,
+	{
+		deserializer.deserialize_str(ColorVis)
+	}
+}
+
+struct ColorVis;
+
+impl Visitor<'_> for ColorVis {
+	type Value = Color;
+
+	fn expecting(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		f.write_str("a mayfig color")
+	}
+
+	fn visit_str<E: serde::de::Error>(self, v: &str) -> Result<Self::Value, E> {
+		let hex = hex_color(v)
+			.ok_or_else(|| serde::de::Error::custom(format_args!("invalid hex color {:?}", v)))?;
+		Ok(Color(hex))
+	}
+}
+
+fn hex_digit(c: u8) -> Option<u8> {
+	match c {
+		b'0'..=b'9' => Some(c - b'0'),
+		b'A'..=b'F' => Some(c - b'A' + 10),
+		b'a'..=b'f' => Some(c - b'a' + 10),
+		_ => None,
+	}
+}
+
+fn hex_color(s: &str) -> Option<[u8; 3]> {
+	let hex = s.strip_prefix("#")?;
+	if let [r1, r2, g1, g2, b1, b2] = hex.as_bytes() {
+		let color = [
+			hex_digit(*r1)? * 16 + hex_digit(*r2)?,
+			hex_digit(*g1)? * 16 + hex_digit(*g2)?,
+			hex_digit(*b1)? * 16 + hex_digit(*b2)?,
+		];
+		Some(color)
+	} else {
+		None
 	}
 }
