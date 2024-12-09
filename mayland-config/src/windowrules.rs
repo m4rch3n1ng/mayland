a66ba1abffadd6e8a4b49c6a24ac6fafd9138de8
may <m4rch3n1ng@gmail.com>
2024-12-09 16:31:14 +0100

    implement regex matching for windowrules
    you can opt into regex matching by pre- and appending the suffix with a
    slash like "/regex/", similar to javascript. without both of the slashes
    mayland will still do a plain full-string match.

--- mayland-config/src/windowrules.rs
+++ mayland-config/src/windowrules.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use serde::{de::Visitor, Deserialize};
 
 #[derive(Debug, Default)]
@@ -54,19 +55,78 @@ pub struct WindowRule {
 #[derive(Debug, Deserialize, PartialEq, Eq)]
 #[serde(rename_all = "snake_case")]
 pub enum Matcher {
-	AppId(String),
-	Title(String),
-	Match(String, String),
+	AppId(Match),
+	Title(Match),
+	Match(Match, Match),
 }
 
 impl Matcher {
 	fn r#match(&self, app_id: Option<&str>, title: Option<&str>) -> bool {
 		match self {
-			Matcher::AppId(a) => app_id.is_some_and(|app_id| a == app_id),
-			Matcher::Title(t) => title.is_some_and(|title| t == title),
+			Matcher::AppId(a) => app_id.is_some_and(|app_id| a.r#match(app_id)),
+			Matcher::Title(t) => title.is_some_and(|title| t.r#match(title)),
 			Matcher::Match(a, t) => {
-				app_id.is_some_and(|app_id| a == app_id) && title.is_some_and(|title| t == title)
+				app_id.is_some_and(|app_id| a.r#match(app_id)) && title.is_some_and(|title| t.r#match(title))
 			}
 		}
 	}
 }
+
+#[derive(Debug)]
+pub enum Match {
+	Regex(Regex),
+	Plain(String),
+}
+
+impl Match {
+	fn r#match(&self, haystack: &str) -> bool {
+		match self {
+			Match::Regex(regex) => regex.is_match(haystack),
+			Match::Plain(plain) => plain == haystack,
+		}
+	}
+}
+
+impl<'de> Deserialize<'de> for Match {
+	fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+		deserializer.deserialize_str(MatchVis)
+	}
+}
+
+struct MatchVis;
+
+impl Visitor<'_> for MatchVis {
+	type Value = Match;
+
+	fn expecting(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		f.write_str("a valid matcher")
+	}
+
+	fn visit_str<E: serde::de::Error>(self, v: &str) -> Result<Self::Value, E> {
+		if let Some(regex) = strip_prefix_suffix(v, '/') {
+			let regex = Regex::new(regex).map_err(serde::de::Error::custom)?;
+			Ok(Match::Regex(regex))
+		} else {
+			let plain = Match::Plain(v.to_owned());
+			Ok(plain)
+		}
+	}
+}
+
+fn strip_prefix_suffix(v: &str, s: char) -> Option<&str> {
+	let v = v.strip_prefix(s)?;
+	let v = v.strip_suffix(s)?;
+	Some(v)
+}
+
+impl PartialEq for Match {
+	fn eq(&self, other: &Self) -> bool {
+		match (self, other) {
+			(Match::Regex(r1), Match::Regex(r2)) => r1.as_str() == r2.as_str(),
+			(Match::Plain(p1), Match::Plain(p2)) => p1 == p2,
+			_ => false,
+		}
+	}
+}
+
+impl Eq for Match {}
