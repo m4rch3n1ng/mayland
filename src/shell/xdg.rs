aabcd4c61778ab8749c85b69e8f79e12d7bdd6e7
may <m4rch3n1ng@gmail.com>
2024-12-14 17:30:27 +0100

    improve code in `handle_surface_commit`
    remove a single arm match and collapse two if statements into one

--- src/shell/xdg.rs
+++ src/shell/xdg.rs
@@ -103,24 +103,19 @@ impl Mayland {
 
 		// handle popup commits.
 		self.popups.commit(surface);
-		if let Some(popup) = self.popups.find_popup(surface) {
-			match popup {
-				PopupKind::Xdg(ref xdg) => {
-					let initial_configure_sent = with_states(surface, |states| {
-						states
-							.data_map
-							.get::<XdgPopupSurfaceData>()
-							.unwrap()
-							.lock()
-							.unwrap()
-							.initial_configure_sent
-					});
-					if !initial_configure_sent {
-						// NOTE: this should never fail as the initial configure is always allowed.
-						xdg.send_configure().expect("initial configure failed");
-					}
-				}
-				PopupKind::InputMethod(ref _input_method) => {}
+		if let Some(PopupKind::Xdg(ref xdg)) = self.popups.find_popup(surface) {
+			let initial_configure_sent = with_states(surface, |states| {
+				states
+					.data_map
+					.get::<XdgPopupSurfaceData>()
+					.unwrap()
+					.lock()
+					.unwrap()
+					.initial_configure_sent
+			});
+			if !initial_configure_sent {
+				// NOTE: this should never fail as the initial configure is always allowed.
+				xdg.send_configure().expect("initial configure failed");
 			}
 		}
 	}
