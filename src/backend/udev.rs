m4rch3n1ng/mayland

commit 0d3434c97029465d8422425bb3e128223bbc52c6

move send_frame_callbacks into on_vblank
rename post_repaint into send_frame_callbacks as that is a significantly
more descriptive name and move the call to that into the on_vblank fn,
but only if the output is not immediately queued for redraw.

this also contains a drive-by bugfix where i immediately requeued on
literally every single vblank because i am sometimes kinda stupid.

src/backend/udev.rs
@@ -148,9 +148,6 @@ impl Udev {
 
 		match drm_compositor.render_frame(&mut device.glow, elements, [0.; 4], FrameFlags::DEFAULT) {
 			Ok(render_output_res) => {
-				// todo put into on_vblank
-				mayland.post_repaint(output);
-
 				if render_output_res.is_empty {
 					return;
 				}
@@ -559,9 +556,11 @@ impl Udev {
 			.clone();
 
 		let output_state = mayland.output_state.get_mut(&output).unwrap();
-		output_state.queued.on_vblank();
+		let requeued = output_state.queued.on_vblank();
 
-		mayland.queue_redraw(output);
+		if !requeued {
+			mayland.send_frame_callbacks(&output);
+		}
 	}
 }
 

src/state.rs
@@ -240,13 +240,15 @@ impl QueueState {
 		}
 	}
 
-	pub fn on_vblank(&mut self) {
+	pub fn on_vblank(&mut self) -> bool {
 		if let QueueState::WaitingForVBlank { queued } = *self {
 			if queued {
-				*self = QueueState::Queued
+				*self = QueueState::Queued;
 			} else {
-				*self = QueueState::Idle
+				*self = QueueState::Idle;
 			}
+
+			queued
 		} else {
 			unreachable!()
 		}
@@ -539,7 +541,7 @@ impl Mayland {
 		output_presentation_feedback
 	}
 
-	pub fn post_repaint(&self, output: &Output) {
+	pub fn send_frame_callbacks(&self, output: &Output) {
 		for mapped in self.workspaces.windows_for_output(output) {
 			mapped
 				.window

src/backend/winit.rs
@@ -111,7 +111,7 @@ impl Winit {
 
 		self.backend.submit(Some(&[damage])).unwrap();
 
-		mayland.post_repaint(output);
+		mayland.send_frame_callbacks(output);
 
 		// ask for redraw to schedule new frame.
 		self.backend.window().request_redraw();

commit 59ef7577f67878b799577776e0df9f6887b12f58

unconditionally call send_frame_callbacks
the call to send_frame_callbacks is still in on_vblank in udev, but it
is now always called instead of only being when the output was not
immediately queued again.

this fixes a regression caused by 0d3434c, where window resizing became
extremely laggy. there may be another underlying issue to this, but for
now everything seems to work as intended with this fix.

src/backend/udev.rs
@@ -556,11 +556,9 @@ impl Udev {
 			.clone();
 
 		let output_state = mayland.output_state.get_mut(&output).unwrap();
-		let requeued = output_state.queued.on_vblank();
+		output_state.queued.on_vblank();
 
-		if !requeued {
-			mayland.send_frame_callbacks(&output);
-		}
+		mayland.send_frame_callbacks(&output);
 	}
 }
 

src/state.rs
@@ -240,15 +240,13 @@ impl QueueState {
 		}
 	}
 
-	pub fn on_vblank(&mut self) -> bool {
+	pub fn on_vblank(&mut self) {
 		if let QueueState::WaitingForVBlank { queued } = *self {
 			if queued {
 				*self = QueueState::Queued;
 			} else {
 				*self = QueueState::Idle;
 			}
-
-			queued
 		} else {
 			unreachable!()
 		}