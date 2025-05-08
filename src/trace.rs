b37b6f3094b23cf6ec725caf79a80121a17f8f56
may <m4rch3n1ng@gmail.com>
2025-05-08 23:05:46 +0200

    switch away from tracing-subscriber
    tracing-subscriber pulls in a lot of bullshit like lazy_static, a second
    version of regex-automata and nu-ansi-term for exactly no reason, so i
    do want to avoid it.
    
    the env_logger crate is not really completely to my needs either: it
    doesn't allow me to easily write to multiple sources and writing to
    journald is hard as well, but it's not that hard to just directly depend
    on env_filter and just handroll most of the rest myself.
    
    this also removes the "debug" feature. by having that behind a
    compile-time feature flag, i lose a lot of the benefit of having the
    ability to get debug logs: i can't just tell someone to "enable the debug
    logs," i have to tell them to recompile mayland first, which is a lot
    more of a user burden than just being able to enable it at runtime. this
    commit removes the debug flag entirely, but i do intend to bring it back
    in the next commit.
    
    similar to the "debug" feature this commit also removes the "trace"
    feature, as having to recompile mayland with it is annoying for the exact
    same reasons. unlike the "debug" flag, i don't think the trace logs are
    actually really useful for like. anything at all, so i don't really have
    a reason to enable it again.
    
    additionally you can enable manually enable trace logs again by
    specifying a custom RUST_LOG flag, but unlike the previous "trace"
    feature, that doesn't split the trace logs into a seperate `.trace.log`
    and regular `.log` file. if i ever do need trace logs, then i can add
    some way to have trace logs again, but for now i don't need it.

--- src/trace.rs
+++ src/trace.rs
@@ -1,98 +1,131 @@
+use anstyle as style;
+use env_filter::Filter;
 use jiff::{Timestamp, Zoned, tz::TimeZone};
-use std::fs::{self, File};
-use tracing::Subscriber;
-use tracing_journald as journald;
-use tracing_subscriber::{
-	EnvFilter, Layer, fmt,
-	layer::{Filter, SubscriberExt},
-	registry::LookupSpan,
-	util::SubscriberInitExt,
+use log::Log;
+use std::{
+	fs::{self, File},
+	io::Write,
 };
+use systemd_journal_logger::JournalLog;
 
 fn iso8601() -> String {
 	let stamp = Timestamp::now();
 	let zoned = Zoned::new(stamp, TimeZone::UTC);
-	zoned.strftime("%Y-%m-%dT%H-%M-%S mayland").to_string()
+	zoned.strftime("%Y-%m-%dT%H-%M-%S mayland.log").to_string()
 }
 
-#[cfg(not(feature = "debug"))]
-const DEFAULT_LOG_FILTER: &str = "warn,mayland=debug,tracing_panic";
-#[cfg(feature = "debug")]
-const DEFAULT_LOG_FILTER: &str = "debug";
-
-fn default_filter<F>() -> impl Filter<F> {
-	let directives = std::env::var("RUST_LOG").unwrap_or_else(|_| DEFAULT_LOG_FILTER.to_owned());
-	EnvFilter::builder().parse_lossy(directives)
-}
-
-#[cfg(feature = "trace")]
-fn only_trace<F>() -> impl Filter<F> {
-	tracing_subscriber::filter::filter_fn(|meta| meta.level() == &tracing::Level::TRACE)
-}
-
-fn log_file(ext: &str) -> File {
+fn log_file() -> File {
 	let local = dirs::data_dir().unwrap_or_else(std::env::temp_dir);
 
 	let dir = local.join("mayland");
 	fs::create_dir_all(&dir).unwrap();
 
 	let date = iso8601();
-	let path = dir.join(date + ext);
+	let path = dir.join(date);
 
-	// todo maybe handle
 	File::create(path).unwrap()
 }
 
-fn with_file<F>() -> impl Layer<F>
-where
-	F: Subscriber + for<'span> LookupSpan<'span>,
-{
-	let file = log_file(".log");
-
-	fmt::layer()
-		.with_writer(file)
-		.with_ansi(false)
-		.with_filter(default_filter())
+struct Logger {
+	filter: Filter,
+	file: File,
+	journald: Option<JournalLog>,
 }
 
-#[cfg(feature = "trace")]
-fn with_trace_file<F>() -> impl Layer<F>
-where
-	F: Subscriber + for<'span> LookupSpan<'span>,
-{
-	let file = log_file(".trace.log");
+impl Logger {
+	fn new() -> Logger {
+		let directive = "warn,mayland=debug,tracing_panic";
 
-	fmt::layer()
-		.with_writer(file)
-		.with_ansi(false)
-		.with_filter(only_trace())
+		let filter = std::env::var("RUST_LOG");
+		let filter = filter.as_deref().unwrap_or(directive);
+		let filter = env_filter::Builder::new().parse(filter).build();
+
+		let file = log_file();
+		let journald = JournalLog::new()
+			.map(|journal| journal.with_syslog_identifier("mayland".to_owned()))
+			.ok();
+
+		Logger {
+			filter,
+			file,
+			journald,
+		}
+	}
+
+	fn write<W: std::io::Write>(
+		&self,
+		mut w: W,
+		color: bool,
+		record: &log::Record<'_>,
+		ts: jiff::Timestamp,
+	) -> Result<(), std::io::Error> {
+		let brace_style = if color {
+			style::AnsiColor::BrightBlack.on_default()
+		} else {
+			style::Style::new()
+		};
+
+		write!(w, "{brace_style}[{brace_style:#}")?;
+		write!(w, "{ts:.3}")?;
+
+		if color {
+			let style = match record.level() {
+				log::Level::Trace => style::AnsiColor::Cyan.on_default(),
+				log::Level::Debug => style::AnsiColor::Blue.on_default(),
+				log::Level::Info => style::AnsiColor::Green.on_default(),
+				log::Level::Warn => style::AnsiColor::Yellow.on_default(),
+				log::Level::Error => style::AnsiColor::Red.on_default().effects(style::Effects::BOLD),
+			};
+
+			write!(w, " {style}{:<5}{style:#}", record.level())?;
+		} else {
+			write!(w, " {:<5}", record.level())?;
+		}
+
+		if record.target() != "" {
+			write!(w, " {}", record.target())?;
+		}
+
+		write!(w, "{brace_style}]{brace_style:#}")?;
+		writeln!(w, " {}", record.args())?;
+
+		Ok(())
+	}
+}
+
+impl Log for Logger {
+	fn enabled(&self, metadata: &log::Metadata<'_>) -> bool {
+		self.filter.enabled(metadata)
+	}
+
+	fn log(&self, record: &log::Record<'_>) {
+		if !self.filter.matches(record) {
+			return;
+		}
+
+		let timestamp = jiff::Timestamp::now();
+
+		let stderr = std::io::stderr().lock();
+		let _ = self.write(stderr, true, record, timestamp);
+
+		let _ = self.write(&self.file, false, record, timestamp);
+
+		if let Some(journald) = &self.journald {
+			journald.log(record);
+		}
+	}
+
+	fn flush(&self) {
+		let _ = (&self.file).flush();
+	}
 }
 
 pub fn setup() {
-	let registry = tracing_subscriber::registry();
+	let logger = Logger::new();
+	let max_level = logger.filter.filter();
 
-	let stderr = fmt::layer()
-		.with_writer(std::io::stderr)
-		.with_filter(default_filter());
-	let registry = registry.with(stderr);
-
-	let file = with_file();
-	let registry = registry.with(file);
-
-	#[cfg(feature = "trace")]
-	let trace_file = with_trace_file();
-	#[cfg(feature = "trace")]
-	let registry = registry.with(trace_file);
-
-	match journald::layer() {
-		Ok(journald) => {
-			let journald = journald.with_filter(default_filter());
-			let registry = registry.with(journald);
-
-			registry.init();
-		}
-		Err(_) => registry.init(),
-	}
+	log::set_boxed_logger(Box::new(logger)).unwrap();
+	log::set_max_level(max_level);
 
 	log_panics::init();
 }
